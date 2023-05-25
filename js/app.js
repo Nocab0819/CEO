16ece49bea8e63d2913bdeb6394796833d3718de
Nocab0819 <penny013101310131@gmail.com>
2023-05-25 23:26:40 +0800

    導覽列（含html和React）
    但不知道為啥導覽列背景色一下有一下沒有

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css'; // 导入自定义样式文件
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
