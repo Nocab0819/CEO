e1e1c8bb66871ec006098d131fa3a71d019629e0
蔡沛恩 <nocab@caipeiendeMacBook-Pro.local>
2023-05-27 00:08:32 +0800

    footer & card

--- js/Footer.js
+++ js/Footer.js
@@ -1,5 +1,13 @@
-<footer class="footer py-2 bg-light">
-  <div class="container text-center">
-    <small class="text-black-50 fw-light mb-0">六角學院 React 課程專屬文件</small>
-  </div>
-</footer>
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer py-2">
+      <div className="container text-center">
+        <small className="w-light mb-0 yellow">本報告不提供任何投資建議。所有資料僅供信息和教育目的。不應基於此處提供的信息做出任何投資決策，您對自己的投資決策完全負責。</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
