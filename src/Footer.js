45306276e87344529d50bab4ea973b31241173c0
蔡沛恩 <penny013101310131@gmail.com>
2023-06-04 12:47:30 +0800

    新增loding圖示、Footer在最上層

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="footer py-2 background-blue">
+    <footer className="footer py-2 background-blue" style={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 9999 }}>
       <div className="footer-container text-center">
         <small className="fw-light mb-0 yellow">本報告不提供任何投資建議。所有資料僅供信息和教育目的。<br />不應基於此處提供的信息做出任何投資決策，您對自己的投資決策完全負責。</small>
       </div>
--- src/lesson.js
+++ src/lesson.js
@@ -1,11 +1,16 @@
-function Lesson({ url}) {
-    return (
-        <div className="container-fluid">
-        <div className='py-md-3 pl-md-5 bd-content' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '764px' }}>
-            <iframe src={url} style={{ width: '800px', height: '100%', border: 'none' }}></iframe>
+function Lesson({ url }) {
+  return (
+    <div style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div class="d-flex justify-content-center margin-top">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden"></span>
+          </div>
         </div>
       </div>
-    );
-  }
-  
-  export default Lesson;
+      <div className='py-md-3 pl-md-5 bd-content' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'calc(100vh - 118px)' }}>
+        <iframe src={url} style={{ width: '800px', height: '100%', border: 'none', position: 'relative', zIndex: 1 }}></iframe>
+      </div>
+    </div>
+  );
+}
