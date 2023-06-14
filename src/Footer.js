7124c128c18b682a5124af7d4934b60b28122ddc
蔡沛恩 <penny013101310131@gmail.com>
2023-06-14 22:45:59 +0800

    首頁圖片調整

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,12 @@
 function Footer() {
-    return (
-      <footer className="alert alert-warning alert-dismissible fade show background-blue" role="alert" style={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 9999 }}>
-        <div className="footer-container text-center">
-          <small className="fw-light mb-0 yellow footer-text">本報告不提供任何投資建議。所有資料僅供信息和教育目的。不應基於此處提供的信息做出任何投資決策，您對自己的投資決策完全負責。</small>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      </footer>
-    );
-  }
+  return (
+    <footer className="alert alert-warning alert-dismissible fade show" role="alert" style={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 9999, backgroundColor: 'hsl(212, 43%, 47%, 0.8)' }}>
+      <div className="footer-container text-center">
+        <small className="fw-light mb-0 yellow footer-text">本報告不提供任何投資建議。所有資料僅供信息和教育目的。不應基於此處提供的信息做出任何投資決策，您對自己的投資決策完全負責。</small>
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </footer>
+  );
+}
